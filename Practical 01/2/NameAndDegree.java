34b64a9206a720691d06e633b895015857d4ce34
Dewaraja I K <132426481+IKDewaraja@users.noreply.github.com>
2023-07-31 12:20:02 +0530

    updated1

---
+++ Practical 01/2/NameAndDegree.java
@@ -0,0 +1,6 @@
+public class NameAndDegree {
+    public static void main(String[] args) {
+        System.out.println("Imandi Dewaraja");
+        System.out.println("BSc (Hons) in Software Engineering");
+    }
+}
